cb50a02f24c251c7bdb046a99f6d39f0c2ce6024
Variares1 <sebastien.romano@viacesi.fr>
2019-09-24 17:48:07 +0200

    class CAD

--- src/model/CAD.java
+++ src/model/CAD.java
@@ -1,27 +1,44 @@
 package model;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
 
 public class CAD {
     public /*DataSet*/void GetRows(String rq_sql, String resultSetName){
+        //"SELECT * FROM tb_personne WHERE pseudo = ? && mdp = ?" = rq_sql
 
-        int foovalue = 500;
-        int pseudor;
-        int mdpr;
-        PreparedStatement st = db.prepareStatement("SELECT * FROM tb_personne WHERE pseudo = "pseudor"&& mdp = "mdpr");
-        st.setInt(1, valeur);
-        ResultSet rs = st.executeQuery();
-        while (rs.next()) {
-            System.out.print("Colonne 1 renvoyée ");
-            System.out.println(rs.getString(1));
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(/*MYSQL_URL*/"", /*DB_USER*/"", /*DB_PASS*/"");
+        }catch (SQLException e){
+            System.out.println("sql error " + e);
         }
-        rs.close();
-        st.close();
+        int foovalue = 500;
+        String mdpr = null; // mp de la personne
+        ResultSet rs = null;
+        if(conn != null){
+            try(PreparedStatement st = conn.prepareStatement(rq_sql)){
+                st.setString(1, resultSetName);
+                st.setString(2, mdpr);
+                rs = st.executeQuery();
+                while (rs.next()) {
+                    System.out.print("Colonne 1 renvoyée ");
+                    System.out.println(rs.getString(1));
+                }
+            }catch (SQLException e){
+                System.out.println("sql error" + e);
+            }finally {
+                try {
+                    if(rs!= null){
+                        rs.close();
+                    }
+                }catch (SQLException e){
+                    System.out.println("sql error " + e);
+                }
 
-        try {
-            PreparedStatement verif = con.prepareStatement("");
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
         }
     }
     public void ActionRows(String rq_sql){
