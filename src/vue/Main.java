0bc35f06ca9d9c82551e6d8e308c3f71081952c4
Lucas <lucas.riviere@viacesi.fr>
2019-09-25 10:43:14 +0200

    IHM Decrptage

--- src/vue/Main.java
+++
@@ -1,9 +0,0 @@
-package src.vue;
-
-public class Main {
-      public static void main(String[] args) {
-            frm_auth tst= new frm_auth();
-
-            frm_auth.connectionDB();
-      }
-}
--- src/vue/frm_auth.java
+++ src/vue/frm_auth.java
@@ -1,16 +1,11 @@
 package vue;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class frm_auth extends JFrame{
             private JPanel pan=new JPanel();
             private JFormattedTextField login=new JFormattedTextField();
-            private JFormattedTextField password=new JFormattedTextField();
+            private JFormattedTextField password=new JFormattedTextField
             private JLabel Llogin=new JLabel("Identifiant");
             private JLabel LPassword=new JLabel("Mot de passe");
             private JLabel titre=new JLabel("<html>Mad Max<br></html>");
@@ -51,35 +46,6 @@ public class frm_auth extends JFrame{
                   pan.add(btn, BorderLayout.SOUTH);
                   this.setContentPane(pan);
                   this.setVisible(true);
-            }
-
-            public String getLogin(){
-                  return login.getText();
-            }
-            public String getPassword(){
-                  return password.getText();
-            }
-            public class BoutonListener implements ActionListener{
-                  @Override
-                  public void actionPerformed(ActionEvent actionEvent) {
-
-                  }
-            }
-
-            public static void connectionDB(){
-                  try{
-                        Class.forName("com.mysql.jdbc.Driver");
-
-                        String url="jdbc:mysql://localhost:3306/RPG";
-                        String user="rekiha";
-                        String pass="Ihgsv5358";
-
-                        Connection con= DriverManager.getConnection(url,user,pass);
-
-                        System.out.println("OK");
-                  } catch (ClassNotFoundException | SQLException e) {
-                        e.printStackTrace();
-                  }
             }
 
 }
